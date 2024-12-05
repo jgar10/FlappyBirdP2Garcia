b87a325192eb07ee5673b2947c3e5d9870068a19
jgar10 <751629@student.dusd.net>
2024-12-05 10:07:23 -0800

    Put floor sky and sprite
    cool

---
+++ P2GarciaFinal/Assets/Scripts/bird.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bird : MonoBehaviour { 
+
+    private bool isDead = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDead == false)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+            }
+        }
+    }
+}    
